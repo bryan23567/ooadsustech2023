63cdceb2d27732a5c6eed6e9d3dfade0a31c8038
MaClazz <sustech.file@gmail.com>
2023-10-03 13:00:24 +0800

    can't display building number in submitted point yet

--- dashboard/coordinates.js
+++ dashboard/coordinates.js
@@ -1,9 +1,10 @@
 const backgroundImage = document.getElementById('background-image');
-const markedPoints = document.getElementById('marked-points');
+const markedPointsContainer = document.getElementById('marked-points-container');
 const popup = document.getElementById('popup');
 const pointForm = document.getElementById('point-form');
 const coordinates = document.getElementById('coordinates');
 let x, y, imageX, imageY; // Declare x and y variables
+const markedPointsInfo = [];
 
 // Add an event listener for mousemove to display coordinates
 backgroundImage.addEventListener('mousemove', (m) => {
@@ -17,9 +18,9 @@ backgroundImage.addEventListener('mousemove', (m) => {
     // Update and display the coordinates
     coordinates.innerText = `X: ${x}, Y: ${y} | Image X: ${imageX.toFixed(2)}%, Image Y: ${imageY.toFixed(2)}%`;
 
-    // Position the coordinates display near the cursor
+    /* Position the coordinates display near the cursor
     coordinates.style.left = x + 'px';
-    coordinates.style.top = y + 'px';
+    coordinates.style.top = y + 'px';*/
 });
 
 // Add an event listener for click to show the form
@@ -43,26 +44,69 @@ pointForm.addEventListener('submit', (s) => {
     // Get user input from the form
     const pointname = document.getElementById('point-name').value;
     const buildingname = document.getElementById('building-name').value;
+    const buildingnumber = document.getElementById('building-number').value;
     const collegename = document.getElementById('college-name').value;
     const totalfloor = document.getElementById('total-floor').value;
     const description = document.getElementById('building-description').value;
 
+    // Store the information for the marked point
+    const markedPointInfo = {
+        x,
+        y,
+        pointname,
+        buildingname,
+        buildingnumber,
+        collegename,
+        totalfloor,
+        description,
+    };
+
+    // Add the marked point information to the array
+    markedPointsInfo.push(markedPointInfo);
+
     // Create a clickable marked point on the image
-    const markedPoint = document.createElement('div');
+    const markedPoint = document.createElement('a');
     markedPoint.className = 'marked-point';
     markedPoint.style.left = x + 'px';
     markedPoint.style.top = y + 'px';
+    markedPoint.href = buildingnumber + '.html'; // Set the href to the desired page
 
     // Attach additional data to the marked point (e.g., name, description)
+    markedPoint.dataset.buildingNumber = buildingnumber;
     markedPoint.dataset.pointname = pointname;
     markedPoint.dataset.buildingname = buildingname;
     markedPoint.dataset.collegename = collegename;
     markedPoint.dataset.totalfloor = totalfloor;
     markedPoint.dataset.description = description;
+    
+    // Create a span element for the building number text
+    const buildingNumberSpan = document.createElement('span');
+    buildingNumberSpan.className = 'building-number';
+    buildingNumberSpan.innerText = buildingnumber;
 
     // Append the marked point to the container
-    markedPoints.appendChild(markedPoint);
+    markedPointsContainer.appendChild(markedPoint);
+/*
+    // Create a button representing the marked point
+    const markedPointButton = document.createElement('button');
+    markedPointButton.innerText = pointname;
 
+    // Add a click event listener to the button to display the information
+    markedPointButton.addEventListener('click', () => {
+        const infoString = `
+            Point Name: ${pointname}
+            Building Name: ${buildingname}
+            Building Number: ${buildingnumber}
+            College Name: ${collegename}
+            Total Floor: ${totalfloor}
+            Description: ${description}
+        `;
+        alert(infoString);
+    });
+
+    // Append the button to the marked points container
+    markedPointsContainer.appendChild(markedPointButton);
+*/
     // Hide the form
     popup.style.display = 'none';
 
