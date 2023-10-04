eb81b76fe32cb90ad43ca0a43ad6770f95f881bb
Sreyny THA <sreynymeni@gmail.com>
2023-10-05 02:31:19 +0800

    update buttons

--- profile/profile.js
+++ profile/profile.js
@@ -1,6 +1,18 @@
 
 
-function sendUserInfo(){
+const btnUpdateInfo = document.querySelector("#showupdate");
+const btnClose = document.querySelector(".close-btn");
+const bgPopUp = document.querySelector(".bg-pop-up");
 
-    const fullName = document.querySelector()
+btnUpdateInfo.addEventListener("click", showPopUp);
+btnClose.addEventListener("click", closePopUp);
+
+function showPopUp() {
+  document.querySelector(".popup").classList.add("active");
+  bgPopUp.style.display = "block";
+}
+
+function closePopUp() {
+  document.querySelector(".popup").classList.remove("active");
+  bgPopUp.style.display = "none";
 }
